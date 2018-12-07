6a2f3cd7d7b382e497b3adb97d856c5c81b78fa3
Silvia García Cacho <silvia@garca.info>
2018-12-07 09:46:01 +0100

    Start JS structure

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
-console.log('>> Ready :)');
+const inputSelector= document.querySelector('.main__input');
+const btnSelector= document.querySelector('.main__btn');
+const divSelector= document.querySelector('.main__result');
+
+function searcher(){
+    
+}
+
+btnSelector.addEventListener('click', searcher);
+
+//recoger del input el nick de una usuaria de GitHub. 
+//Al hacer click en el botón "Buscar" 
+//pedir a Github la información que tienen sobre dicha usuaria 
+//De la información que recibamos seleccionaremos el nombre real de la criatura ﴾sin los apellidos﴿ 
+//escribiremos cada letra en un li de una lista ordenada.
