ce6ac1f9d49bbb89521bf9c82f4b5a0fe723787a
Silvia García Cacho <silvia@garca.info>
2018-12-08 20:36:00 +0100

    Add new error messages

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -5,30 +5,51 @@ const btnSelector= document.querySelector('.main__btn');
 const listSelector= document.querySelector('.result__list');
 
 function searcher(){
+    //pedir a Github la información que tienen sobre dicha usuaria
     fetch(`https://api.github.com/users/${inputSelector.value}`)
     .then(function(response) {
+        console.log(response);
+        //si la usuaria no existe: sale un mensaje "Lo siento, no hemos encontrado a esta criatura"
+        if(response.status === 404){
+            listSelector.innerHTML = '';
+            listSelector.innerHTML = `<div class="message"> Lo siento, no hemos encontrado a esta criatura en el sistema.</div>`
+        }
         return response.json();
     })
     .then(function(data) { 
-
-        const dataName = data.name;
-        // const dataArrName = dataName.split(" ");
-        // console.log(dataName.split(" ")[0]);
-        // console.log(dataArrName);
-        // const dataFirstName = dataArrName[0];
-        const dataFirstName = dataName.split(" ")[0];
-        console.log(dataFirstName);
-        for (let i=0; i<dataFirstName.length; i++){
-            listSelector.innerHTML += `<li class="result__box"> ${dataFirstName[i]}</li>`;
+        
+        //si la usuaria no tiene puesto el nombre completo?: sale un mensaje "Lo siento, esta criatura prefiere permanecer en el anonimato"
+        if (data.name === null){
+            listSelector.innerHTML = '';
+            listSelector.innerHTML = `<div class="message">Lo siento, esta criatura prefiere permanecer en el anonimato.</div>`
         }
+        //De la información que recibamos seleccionaremos el nombre real de la criatura ﴾sin los apellidos﴿
+        else { 
+            const dataName = data.name;
+            const dataFirstName = dataName.split(" ")[0];
+            console.log(dataFirstName);
+            listSelector.innerHTML = '';
+            //escribiremos cada letra en un li de una lista ordenada.
+            for (let i=0; i<dataFirstName.length; i++){
+                listSelector.innerHTML += `<li class="result__box"> ${dataFirstName[i]}</li>`;
+            }
+        }
+        
     });
     
 } 
 
+//recoger del input el nick de una usuaria de GitHub. 
+//Al hacer click en el botón "Buscar"
 btnSelector.addEventListener('click', searcher);
 
-//recoger del input el nick de una usuaria de GitHub. 
-//Al hacer click en el botón "Buscar" 
-//pedir a Github la información que tienen sobre dicha usuaria 
-//De la información que recibamos seleccionaremos el nombre real de la criatura ﴾sin los apellidos﴿ 
-//escribiremos cada letra en un li de una lista ordenada.
+
+
+
+/// Usuario que AHORA NO EXISTE PERO EXISTIÓ
+/// https://api.github.com/users/asdfasdfa
+
+/// NO EXISTE
+/// https://api.github.com/users/32413ur89sdfasdfa
+ 
+
